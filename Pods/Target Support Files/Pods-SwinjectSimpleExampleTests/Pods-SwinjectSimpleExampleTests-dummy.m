b70aac426aa1bdaf595ba92b3d7eaeaa52164693
koogawa <koogawa.app@gmail.com>
2016-07-17 10:36:11 +0900

    Remove unused files

--- Pods/Target Support Files/Pods-SwinjectSimpleExampleTests/Pods-SwinjectSimpleExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwinjectSimpleExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwinjectSimpleExampleTests
-@end
--- Pods/Target Support Files/Swinject/Swinject-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Swinject.h"
-#import "_SwinjectStoryboardBase.h"
-
-FOUNDATION_EXPORT double SwinjectVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwinjectVersionString[];
-
